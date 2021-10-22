77101e1f1135f15ab3cbb90ba5850dea570f44d3
litch0 <lmtr0@protonmail.ch>
2021-10-22 10:15:30 -0300

    Moved to Polodb, more lightweigh and more space efficient

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,8 @@
-use ejdb::{Database, bson, query::{Q, QH}};
+
+mod _ejdb;
+mod _polo;
 
 fn main() {
-    println!("Hello, world!");
-
-    let db = Database::open("data/mydb").unwrap();
-    let col = db.collection("some_collection").unwrap();
-
-    let mut index: String = String::new();
-    for i in 0..10 {
-        let d = bson! {
-            "name" => "Foo Bar",
-            "count" => i
-        };
-
-        let id = col.save(&d).unwrap();
-        if i == 5 {
-            index = id.to_string()
-        }
-    }
-    // col.index("name").string(false).set().unwrap();
-    println!("Count {:#?}", col.query(Q.empty(), QH.empty()).count().unwrap());
-    
-    { // by id
-        let now = std::time::SystemTime::now();
-        println!("Finding {}", &index);
-        println!("{:#?}", col.query(Q.id(index.as_str()), QH.empty()).find_one().unwrap().unwrap());
-        let since = std::time::SystemTime::now().duration_since(now).unwrap();
-        println!("Took {:?} to find", &since);
-    }
-    
-    { // random field
-        let now = std::time::SystemTime::now();
-        println!("Finding {}", &index);
-        println!("{:#?}", col.query(Q.field("count").eq(1), QH.empty()).find_one().unwrap().unwrap());
-        let since = std::time::SystemTime::now().duration_since(now).unwrap();
-        println!("Took {:?} to find", &since);
-    }
-    
+    // _ejdb::main();
+    _polo::main();
 }
